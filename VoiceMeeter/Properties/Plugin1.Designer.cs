2cb94cfb08b4ee04380a56b70d967752c7c3bff2
BarRaider <46548278+BarRaider@users.noreply.github.com>
2021-06-17 14:27:53 -0700

    v2.0 - Logo update + dependencies

--- VoiceMeeter/Properties/Plugin1.Designer.cs
+++ VoiceMeeter/Properties/Plugin1.Designer.cs
@@ -12,7 +12,7 @@ namespace VoiceMeeter.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Plugin : global::System.Configuration.ApplicationSettingsBase {
         
         private static Plugin defaultInstance = ((Plugin)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Plugin())));
